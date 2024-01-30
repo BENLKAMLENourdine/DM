4941f961cc4540b47337d645d8df224bf537e3cb
NOURDINE BENLKAMLE <benlkamle.tiznit.95@gmail.com>
2024-01-30 03:19:17 +0000

    add some basic head properties & SEO meta tags

--- app.vue
+++ app.vue
@@ -4,3 +4,25 @@
     </NuxtPage>
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'DM - Car Rental',
+  meta: [
+    { charset: "UTF-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+    { name: 'description', content: 'The best car rental platform.' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+useSeoMeta({
+  title: 'DM - Car Rental',
+  ogTitle: 'DM - Car Rental',
+  description: 'The best car rental platform.',
+  ogDescription: 'The best car rental platform.',
+})
+
+</script>
